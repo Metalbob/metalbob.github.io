367fd1c52ff9b397a412750b03b19ecc383e2726
Julien <julien.falcioni.54@gmail.com>
2024-10-13 20:26:18 +0200

    working functions for content placement

--- functions.js
+++ functions.js
@@ -2,15 +2,23 @@ function right_aligned_content(content_path, description)
 {
 	const iframe = document.getElementById("project-description");
 	const section_container = document.createElement("div");
+	section_container.classList.add('aligned-content-container');
 	iframe.appendChild(section_container);
 
 	fetch(content_path)
 		.then(response => {
 			const contentType = response.headers.get('Content-Type');
-			console.log('Le type MIME du fichier est :', contentType);
-			
+
+			var descriptionDiv = document.createElement('div');
+			section_container.appendChild(descriptionDiv);
+			var descriptionContainer = document.createElement('p');
+			descriptionContainer.innerHTML = description;
+			descriptionDiv.appendChild(descriptionContainer);
 			var content_container = null;
-			
+
+			var contentDiv = document.createElement('div');
+			section_container.appendChild(contentDiv);
+
 			// Traiter le fichier en fonction de son type MIME
 			if (contentType.startsWith('image/'))
 			{
@@ -24,11 +32,12 @@ function right_aligned_content(content_path, description)
 			{
 				console.log('Type de fichier non pris en charge.');
 			}
-			
+
 			if (content_container != null)
 			{
 				content_container.src = content_path;
-				section_container.appendChild(content_container);
+				//content_container.style = "float: right";
+				contentDiv.appendChild(content_container);
 			}
 		})
 		.catch(error => console.error('Erreur lors de la récupération du fichier:', error));
@@ -36,5 +45,46 @@ function right_aligned_content(content_path, description)
 
 function left_aligned_content(content_path, description)
 {
-	
+	const iframe = document.getElementById("project-description");
+	const section_container = document.createElement("div");
+	section_container.classList.add('aligned-content-container');
+	iframe.appendChild(section_container);
+
+	fetch(content_path)
+		.then(response => {
+			const contentType = response.headers.get('Content-Type');
+
+			var content_container = null;
+
+			var contentDiv = document.createElement('div');
+			section_container.appendChild(contentDiv);
+
+			// Traiter le fichier en fonction de son type MIME
+			if (contentType.startsWith('image/'))
+			{
+				content_container = document.createElement("img");
+			}
+			else if (contentType.startsWith('video/'))
+			{
+				content_container = document.createElement("video");
+			}
+			else
+			{
+				console.log('Type de fichier non pris en charge.');
+			}
+
+			if (content_container != null)
+			{
+				content_container.src = content_path;
+				//content_container.style = "float: right";
+				contentDiv.appendChild(content_container);
+			}
+
+			var descriptionDiv = document.createElement('div');
+			section_container.appendChild(descriptionDiv);
+			var descriptionContainer = document.createElement('p');
+			descriptionContainer.innerHTML = description;
+			descriptionDiv.appendChild(descriptionContainer);
+		})
+		.catch(error => console.error('Erreur lors de la récupération du fichier:', error));
 };
--- script.js
+++ script.js
@@ -8,18 +8,34 @@ fetch('projects/projects.json')
 			// Créer le bouton pour chaque projet
 			const button = document.createElement('button');
 			button.className = 'timeline-btn';
-			
 			button.innerHTML = `<img src="${project.icon}" alt="${project.title}">`;
 			button.dataset.project = project.year;
-				
+
 			// Ajoute le comportement de clic
 			button.addEventListener('click', () => {
 				// Remplacer le contenu du projet affiché
 				const projectHeader = document.getElementById('project-header');
 				projectHeader.style = `background-image:url(${project.header}); background-position:${project.headercoordinates}`;
 				projectHeader.innerHTML = `<h2>${project.title}</h2>`;
-				const iframe = document.getElementById('project-description');
-				iframe.src = `${project.content}`
+				
+				fetch(`${project.content}`)
+					.then(response => {
+						return response.text();
+					})
+					.then(text => {
+						const projectDescription = document.getElementById('project-description');
+						projectDescription.innerHTML = text;
+
+						const scripts = projectDescription.querySelectorAll('script');
+						scripts.forEach( script => {
+							const newScript = document.createElement('script');
+							newScript.textContent = script.textContent;
+							projectDescription.removeChild(script);
+							projectDescription.appendChild(newScript);
+						});
+
+
+					});
 			});
 
 			// Ajouter le bouton à la frise
@@ -42,15 +58,15 @@ fetch('projects/projects.json')
 			// Créer le bouton pour chaque projet
 			const button = document.createElement('button');
 			button.className = 'timeline-btn';
-			
+
 			button.innerHTML = `<img src="${project.icon}" alt="${project.title}">`;
 			button.dataset.project = project.year;
-				
+
 			// Ajoute le comportement de clic
 			button.addEventListener('click', () => {
 				// Remplacer le contenu du projet affiché
 				projectDetails.innerHTML = `
-					<div class="project-header" style="background-image:url(${project.header}); background-position:${project.headercoordinates}">	
+					<div class="project-header" style="background-image:url(${project.header}); background-position:${project.headercoordinates}">
 						<h2>${project.title}</h2>
 					</div>
 					<iframe class="project-description" src="${project.content}" frameborder="0"></iframe>
